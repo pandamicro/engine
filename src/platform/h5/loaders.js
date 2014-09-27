e9ef9ad0af6081ddb2dfbc20486f723ed1695c0a
Jare Guo <jare85@163.com>
2014-09-27 14:39:24 +0800

    polish

--- src/platform/h5/loaders.js
+++ src/platform/h5/loaders.js
@@ -4,17 +4,30 @@
  */
 function ImageLoader(url, callback, onProgress) {
     var image = document.createElement('img');
-    if (callback) {
-        image.addEventListener('load', function () {
+    image.crossOrigin = 'Anonymous';
+
+    var onload = function () {
+        if (callback) {
             callback(this);
-        }, false);
-        image.addEventListener('error', function (msg, line, url) {
+        }
+        image.removeEventListener('load', onload);
+        image.removeEventListener('error', onerror);
+        image.removeEventListener('progress', onProgress);
+    };
+    var onerror = function (msg, line, url) {
+        if (callback) {
             var error = 'Failed to load image: ' + msg + ' Url: ' + url;
             callback(null, error);
-        }, false);
-    }
+        }
+        image.removeEventListener('load', onload);
+        image.removeEventListener('error', onerror);
+        image.removeEventListener('progress', onProgress);
+    };
+
+    image.addEventListener('load', onload);
+    image.addEventListener('error', onerror);
     if (onProgress) {
-        image.addEventListener('progress', onProgress, false);
+        image.addEventListener('progress', onProgress);
     }
     image.src = url;
     return image;
@@ -26,36 +39,44 @@ function ImageLoader(url, callback, onProgress) {
 function _LoadFromXHR(url, callback, onProgress, responseType) {
     var xhr = new XMLHttpRequest();
     //xhr.withCredentials = true;   // INVALID_STATE_ERR: DOM Exception 11 in phantomjs
-    var total = 0;
+    var total = -1;
     xhr.onreadystatechange = function () {
-        if (callback && xhr.readyState === xhr.DONE) {
-            if (xhr.status === 200 || xhr.status === 0) {
-                callback(xhr);
+        if (xhr.readyState === xhr.DONE) {
+            if (callback) {
+                if (xhr.status === 200 || xhr.status === 0) {
+                    callback(xhr);
+                }
+                else {
+                    callback(null, 'LoadFromXHR: Could not load "' + url + '", status: ' + xhr.status);
+                }
             }
-            else {
-                callback(null, 'LoadFromXHR: Could not load "' + url + '", status: ' + xhr.status);
+            xhr.onreadystatechange = null;
+            if (onProgressEventListener) {
+                xhr.removeEventListener('progress', onProgressEventListener);
             }
         }
         if (onProgress && xhr.readyState === xhr.LOADING && !('onprogress' in xhr)) {
-            if (total === 0) {
+            if (total === -1) {
                 total = xhr.getResponseHeader('Content-Length');
             }
             onProgress(xhr.responseText.length, total);
         }
         if (onProgress && xhr.readyState === xhr.HEADERS_RECEIVED) {
-            total = xhr.getResponseHeader("Content-Length");
+            total = xhr.getResponseHeader('Content-Length');
         }
     };
-    xhr.open("GET", url, true);
+    xhr.open('GET', url, true);
     if (responseType) {
         xhr.responseType = responseType;
     }
+    var onProgressEventListener;
     if (onProgress && 'onprogress' in xhr) {
-        xhr.addEventListener('progress', function (event) {
+        onProgressEventListener = function (event) {
             if (event.lengthComputable) {
                 onProgress(event.loaded, event.total);
             }
-        }, false);
+        };
+        xhr.addEventListener('progress', onprogress);
     }
     xhr.send();
 }
