b4b93de3033caa66be2b4587e7c57d90f3a1b1b7
Jare Guo <jare85@163.com>
2015-02-21 01:12:08 +0800

    close fireball-x/dev#337: implement rotated sprite renderer

--- src/component/sprite-renderer.js
+++ src/component/sprite-renderer.js
@@ -97,16 +97,21 @@
     };
 
     SpriteRenderer.prototype.getWorldSize = function () {
-        var size = new Fire.Vec2(0, 0);
-        size.x = this._sprite ? this._sprite.width : 0;
-        size.y = this._sprite ? this._sprite.height : 0;
-        return size;
+        return new Fire.Vec2(this.width, this.height);
     };
 
     var tempMatrix = new Fire.Matrix23();
 
     SpriteRenderer.prototype.onPreRender = function () {
         this.getSelfMatrix(tempMatrix);
+        if (this._sprite && this._sprite.rotated) {
+            // rotate cw
+            tempMatrix.b = -tempMatrix.d;
+            tempMatrix.c = tempMatrix.a;
+            tempMatrix.a = 0;
+            tempMatrix.d = 0;
+            tempMatrix.tx += this.width;
+        }
         tempMatrix.prepend(this.transform._worldTransform);
         Engine._curRenderContext.updateTransform(this, tempMatrix);
     };
@@ -124,19 +129,40 @@
         var pivotY = 0.5;
         var scaleX = 1;
         var scaleY = 1;
+        //var rotated = false;
         if (Fire.isValid(this._sprite)) {
+            //rotated = this._sprite.rotated;
             pivotX = this._sprite.pivot.x;
             pivotY = this._sprite.pivot.y;
             scaleX = w / this._sprite.width;
             scaleY = h / this._sprite.height;
         }
 
-        out.tx = - pivotX * w;
-        out.ty = (1.0 - pivotY) * h;
-        out.a = scaleX;
-        out.b = 0;
-        out.c = 0;
-        out.d = scaleY;
+        //if ( !rotated ) {
+            out.a = scaleX;
+            out.b = 0;
+            out.c = 0;
+            out.d = scaleY;
+            out.tx = - pivotX * w;
+            out.ty = (1.0 - pivotY) * h;
+        //}
+        //else {
+        //    // CCW
+        //    //out.a = 0;
+        //    //out.b = scaleY;
+        //    //out.c = -scaleX;
+        //    //out.d = 0;
+        //    //out.tx = - (pivotY - 1.0) * w;
+        //    //out.ty = - pivotX * h;
+        //
+        //    // CW
+        //    out.a = 0;
+        //    out.b = -scaleY;
+        //    out.c = scaleX;
+        //    out.d = 0;
+        //    out.tx = (1.0 - pivotX) * w;
+        //    out.ty = (1.0 - pivotY) * h;
+        //}
     };
 
     return SpriteRenderer;
